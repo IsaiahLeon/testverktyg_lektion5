b5ae7197d369396129b8ee25943404aac598910b
IsaiahLeon <isaiah.leon.eriksson@gmail.com>
2023-04-12 14:55:04 +0200

    dagens sista commit

--- main.js
+++ main.js
@@ -124,5 +124,120 @@ let isaiah = {
 console.log(isaiah);
 
 console.log(`First name: ${isaiah.firstName}`);
+
 let isaiahStr = `${isaiah.firstName} ${isaiah.lastName}, age: ${isaiah.age}`;
 console.log(isaiahStr);
+
+let isaiahAdvanced = {
+    firstName: 'Isaiah',
+    lastName: 'Leon',
+    age: 32,
+    hobbies: ['music', 'film', 'hockey', 'lakes']
+};
+
+console.log(isaiahAdvanced); // object
+console.log(JSON.stringify(isaiahAdvanced)); // JSON
+
+//Loop over object
+for(let propertyName in isaiahAdvanced) {
+    let propertyValue = isaiahAdvanced[propertyName];
+    console.log(propertyName, propertyValue);
+}
+
+// Loop through object and array inside of object (kolla Mike's kod/överkurs)
+
+
+// Connection to HTML
+// ask JS to create a new element
+let myDiv = document.createElement('div');
+// add some HTML
+myDiv.innerHTML = `
+    <h1>Hello from JS!</h1>
+    <p>This text is from JavaScript, my age is ${isaiah.age}</p>
+    `;
+
+    //Grab the body tag
+    let body = document.querySelector('body');
+
+    //add the div to the HTML
+    body.append(myDiv);
+
+    // Another way to add data
+    let myBands = [
+        {
+            name: 'The Beatles',
+            genre: 'Pop'
+        },
+        {
+            name: 'The Rolling Stones',
+            genre: 'Rock'
+        }
+    ];
+
+    // Grab the bands div
+    let bandsDiv = document.querySelector('#bands');
+    for(let band of myBands) {
+        bandsDiv.innerHTML += `
+            <div class="band">
+                <h2>${band.name}</h2>
+                <p>Genre: ${band.genre}</p>
+            </div>
+            `;
+    }
+
+    // Functions
+    function sayHello() {
+        console.log('Hello');
+    }
+
+    const sayHello2 = () => {
+        console.log('Hello 2');
+    
+    }
+
+    sayHello();
+    sayHello2();
+
+    // Different ways of declaring the same function: 1. funtion declaration 2. arrow function 3. function expression
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    const add2 = (a, b) => {
+        return a + b;
+    }
+
+    const add3 = function(a, b) {
+        return a + b;
+    }
+
+    //4. arrow function with implicit return
+    const add4 = (a, b) => a + b;
+
+    //5. arrow function with implicit return and one parameter
+    const add5 = a => a + 5;
+
+    console.log(add(1, 2));
+    console.log(add2(1, 2));
+    console.log(add3(1, 2));
+    console.log(add4(1, 2));
+    console.log(add5(1, 2));
+
+    class Person {
+        constructor(name, age, hobby) {
+            this.name = name;
+            this.age = age;
+            this.hobby = hobby;
+        }
+
+        greetings() {
+            console.log(`Hi, my name is ${this.name} and I am ${this.age}`);
+            console.log(`I enjoy: ` + this.hobby);
+        }
+       
+    
+    }
+
+    const isaiahClass = new Person('Isaiah', 32, 'Music');
+    isaiahClass.greetings();
